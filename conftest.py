4163bde5e9e18eca61b373bdca1464b5b9024222
AMullova <anna.mullova@irlix.ru>
2023-02-18 12:55:23 +0400

    изменения

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.firefox.options import Options
+
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default=None,
@@ -11,20 +11,17 @@ def pytest_addoption(parser):
                      
 @pytest.fixture(scope="function")
 def browser(request):
-    user_language = request.config.getoption("language")
     browser_name = request.config.getoption("browser_name")
-    browser = None
+    user_language = request.config.getoption("language")
+    
     options = Options()
-    if browser_name == "chrome":
+    browser = None
+    if  browser_name == "chrome":
         print("\nstart chrome browser for test..")
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         browser = webdriver.Chrome(options=options)
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        options.set_preference("intl.accept_languages", user_language)
-        browser = webdriver.Firefox(options=options)
     else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+        raise pytest.UsageError("--browser_name should be select")
     yield browser
     print("\nquit browser..")
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,9 +1,10 @@
 from selenium.webdriver.common.by import By
+import time
 
-link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_user_see_button(browser):
     browser.get(link)
-    time.sleep(30)
-    assert browser.find_elements(By.CSS-SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket"), 'button is not found'
+    time.sleep(10)
+    assert browser.find_element(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket"), 'button is not found'
 
