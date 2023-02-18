f36f8f825dc5b898123640137ede4bdf3ba920e9
AMullova <anna.mullova@irlix.ru>
2023-02-18 13:00:27 +0400

    Добавлено ожидание

--- test_items.py
+++ test_items.py
@@ -5,6 +5,6 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_user_see_button(browser):
     browser.get(link)
-    time.sleep(10)
+    time.sleep(30)
     assert browser.find_element(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket"), 'button is not found'
 
